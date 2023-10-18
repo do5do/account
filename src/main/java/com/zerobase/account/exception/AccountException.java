3a036deaa8d3f6d73a252768fa51fe1ee396c645
do5do <do151015@gmail.com>
2023-10-18 16:37:39 +0900

    AccountException 수정

--- src/main/java/com/zerobase/account/exception/AccountException.java
+++ src/main/java/com/zerobase/account/exception/AccountException.java
@@ -2,7 +2,9 @@ package com.zerobase.account.exception;
 
 
 import com.zerobase.account.type.ErrorCode;
+import lombok.Getter;
 
+@Getter
 public class AccountException extends RuntimeException {
     private ErrorCode errorCode;
     private String errorMessage;
